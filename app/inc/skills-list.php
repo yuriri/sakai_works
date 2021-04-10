6a6d96409b748f3d7bb06d2df62f5d8e7fe9ed89
sakai <yusasabi@gmail.com>
2021-04-11 04:12:09 +0900

    スキルの表示調整

--- app/inc/skills-list.php
+++ app/inc/skills-list.php
@@ -1,59 +1,138 @@
-<!-- /.m-skills -->
-<article class="m-skills">
+<!-- /.l-skills -->
+<article class="l-skills">
 
     <h2 class="l-ttl_02 ttl-skills">SKILSS</h2>
 
-    <?php 
-        $args = array(
-            'pad_counts' => true,
-            'hide_empty' => true,
-            'parent' => 0        
-        );
-        $terms = get_terms( 'skills_post_cat', $args );
-        foreach ( $terms as $term ) :    
-            
+    <!-- .l-skills_box -->
+    <section class="l-skills_box">
+
+        <?php 
+            $taxonomy = 'skills_post_cat';
             $args = array(
-                'post_status' => 'publish',
-                'post_type' =>  'skills_post',
-                'posts_per_page' => -1,                             
-                'tax_query' => array(
-                    array(
-                        'taxonomy' => 'skills_post_cat',
-                        'field' => 'slug',
-                        'terms' => $term,
-                    ),
-                ),             
-            );  
-
-            $my_query = new WP_Query($args);
-            if ($my_query->have_posts()):            
-    ?>
-
-        <h3 class="l-skills_term"><span><?php echo $term->name; ?></span></h3>
-
-        <ul class="l-skills_list">
-
-            <?php while ($my_query->have_posts()) : $my_query->the_post();
-                $title = get_the_title();
-                $level = get_field('level');
-                $years = get_field('years');
+                'pad_counts' => true,
+                'hide_empty' => true,
+                'parent' => 0        
+            );
+            $terms = get_terms( 'skills_post_cat', $args );
+            //親タームのみ回す
+            foreach ( $terms as $term ) :  
             ?>
+    
+            <!-- .l-skills_container -->
+            <section class="l-skills_container">
+    
+                <h3 class="l-skills_term"><span><?php echo $term->name; ?></span></h3>
+     
+                <?php
+                    $term_id = $term->term_id;
 
-                <li class="l-skills_list_item">
-                    <h4 class="l-skills_list_item_name"><?php echo $title; ?></h4>
-                    <p class="l-skills_list_item_level"><?php echo $level; ?></p>
-                    <p class="l-skills_list_item_years"><?php echo $years; ?></p>
-                </li>
+                    // 子タームを取得する
+                    $termchildren = get_term_children( $term_id, $taxonomy );
+
+                    // 子タームがある場合
+                    if($termchildren):
+                        foreach ( $termchildren as $child ): //その中で子タームを回す
+                            $termC = get_term_by( 'id' , $child, $taxonomy );
+                            $term_link = get_term_link( $child );
+                    
+                            if( $termC->count != 0 ):
+                            ?>
+
+                                <!-- .l-skills_term-child_block -->
+                                <article class="l-skills_term-child_block">
+
+                                    <h4 class="l-skills_term-child_ttl"><?php echo $termC->name; ?></h4>
+
+                                    <ul class="l-skills_list">
+            
+                                        <?php
+                                            $args = array(
+                                                'post_status' => 'publish',
+                                                'post_type' =>  'skills_post',
+                                                'posts_per_page' => -1,                             
+                                                'tax_query' => array(
+                                                    array(
+                                                        'taxonomy' => $taxonomy,
+                                                        'field' => 'slug',
+                                                        'terms' => $termC,
+                                                    ),
+                                                ),             
+                                            );  
+            
+                                            $my_query = new WP_Query($args);                                
+                                            if ($my_query->have_posts()):            
+                                        
+                                                while ($my_query->have_posts()) : $my_query->the_post();
+                                                    $title = get_the_title();
+                                                    $level = get_field('level');
+                                                    $years = get_field('years');    
+                                                ?>
                 
-            <?php
-                endwhile;
-            endif;
-        wp_reset_postdata();
-        ?>  
+                                                    <li class="l-skills_list_item">
+                                                        <h4 class="l-skills_list_item_name"><?php echo $title; ?></h4>
+                                                        <p class="l-skills_list_item_level">レベル：<?php echo $level; ?></p>
+                                                        <p class="l-skills_list_item_years">年数：<?php echo $years; ?></p>
+                                                    </li>
+                                    
+                                                <?php
+                                                endwhile;
+                                            endif;
+                                        wp_reset_postdata();
+                                        ?>
+                                    </ul>
 
-        </ul>
+                                </article>
+                                <!-- /.l-skills_term-child_block -->
+                            <?php endif; ?>
+                        <?php endforeach;
+                    else: 
+                    // 子タームが無い場合    
+                ?>
+                    <ul class="l-skills_list">
+                        
+                        <?php
+                            $args = array(
+                                'post_status' => 'publish',
+                                'post_type' =>  'skills_post',
+                                'posts_per_page' => -1,                             
+                                'tax_query' => array(
+                                    array(
+                                        'taxonomy' => $taxonomy,
+                                        'field' => 'slug',
+                                        'terms' => $term,
+                                    ),
+                                ),             
+                            );  
 
-    <?php endforeach; ?> 
+                            $my_query = new WP_Query($args);                                
+                            if ($my_query->have_posts()):            
+                        
+                                while ($my_query->have_posts()) : $my_query->the_post();
+                                    $title = get_the_title();
+                                    $level = get_field('level');
+                                    $years = get_field('years');    
+                                ?>
+                                    <li class="l-skills_list_item">
+                                        <h4 class="l-skills_list_item_name"><?php echo $title; ?></h4>
+                                        <p class="l-skills_list_item_level">レベル：<?php echo $level; ?></p>
+                                        <p class="l-skills_list_item_years">年数：<?php echo $years; ?></p>
+                                    </li>                                
+                                <?php
+                                endwhile;
+                            endif;
+                        wp_reset_postdata();
+                        ?>                                
+                    </ul>
+
+                <?php endif; ?>  
+    
+            </section>
+            <!-- /.l-skills_container -->
+                    
+        <?php endforeach; ?> 
+
+    </section>
+    <!-- /.l-skills_box -->
             
 </article>
-<!-- /.m-skills -->
+<!-- /.l-skills -->
