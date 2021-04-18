b187efaeffba71ea09335889151e607fff5efb15
sakai <yusasabi@gmail.com>
2021-04-19 03:14:09 +0900

    aboutの投稿文章表示とスタイル調整

--- app/inc/about.php
+++ app/inc/about.php
@@ -8,7 +8,17 @@
             <span class="l-about_ttl_me">me...</span>
         </h2>
     
-        <p class="l-about_text">フロントエンドエンジニア、マークアップエンジニア、時々デザイン。フロントエンドエンジニア、マークアップエンジニア、時々デザイン。フロントエンドエンジニア、マークアップエンジニア、時々デザイン。フロントエンドエンジニア、マークアップエンジニア、時々デザイン。フロントエンドエンジニア、マークアップエンジニア、時々デザイン。フロントエンドエンジニア、マークアップエンジニア、時々デザイン。フロントエンドエンジニア、マークアップエンジニア、時々デザイン。フロントエンドエンジニア、マークアップエンジニア、時々デザイン。フロントエンドエンジニア、マークアップエンジニア、時々デザイン。</p>
+        <?php 
+            $args = array(
+                'post_type' => 'about',
+                'post_status' => 'publish',
+                'posts_per_page' => 1
+            );
+            $wp_query = new WP_Query($args);  
+            if ( $wp_query -> have_posts() ) :while ( $wp_query -> have_posts() ) : $wp_query -> the_post();
+        ?>
+            <div class="l-about_text"><?php the_content(); ?></div>
+        <?php endwhile;endif; ?>
 
         <button class="l-btn_scroll"><span>SCROLL</span><svg xmlns="http://www.w3.org/2000/svg" width="11.413" height="79.677"><path d="M.25 0v79.07l10.986-11.092" fill="none" stroke="#000" stroke-width=".5"/></svg></button>
 
